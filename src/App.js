9556e5e6173e5973531b99c8932023096d4510b0
kicikhaluk <f.halukkicik@outlook.com>
2021-01-07 23:34:18 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 const App = () => {
+
   return (
-    <div>
-      Hello World
-    </div>
+    <Layout />
   );
 }
 
---
+++ src/components/UI/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = ({ name }) => {
+  return (
+    <>
+      <h1>{name}</h1>
+    </>
+  );
+};
+
+export default Card;
